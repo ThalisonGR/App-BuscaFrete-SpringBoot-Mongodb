69e3af968fc0fe4a9e7eacb27f8a58f2342676fd
Thalison <Tec.thalison@gmail.com>
2024-01-23 06:23:38 -0300

    :wrench: chore: Adição do springdoc-openapi-starter-webmvc-ui

--- BuscaFrete/src/main/java/br/com/buscafrete/BuscaFrete/controllers/MotoristaController.java
+++ BuscaFrete/src/main/java/br/com/buscafrete/BuscaFrete/controllers/MotoristaController.java
@@ -3,14 +3,13 @@ package br.com.buscafrete.BuscaFrete.controllers;
 import br.com.buscafrete.BuscaFrete.domain.motorista.Motorista;
 import br.com.buscafrete.BuscaFrete.domain.motorista.MotoristaDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value ="/api/motorista")
 public class MotoristaController {
 
+    @PostMapping(value = "/inserir")
     public ResponseEntity<Motorista> iserir (@RequestBody MotoristaDTO motoristaDTO){
     return null;
     }
